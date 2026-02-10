f9b630531b624b9ba75e0d0388a98de612501c88
Ahmed-Sinjab <ahmed.sinjabas@gmail.com>
2026-02-09 19:33:28 -0500

    added a repo for users

---
+++ FitTrack/src/main/java/com/FitnesTracker/FitTrack/fitTrackAPI/repo/UserService.java
@@ -0,0 +1,18 @@
+package com.FitnesTracker.FitTrack.fitTrackAPI.repo;
+
+
+import com.FitnesTracker.FitTrack.fitTrackAPI.domain.workout;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.FitnesTracker.FitTrack.fitTrackAPI.domain.workout;
+
+import java.util.Optional;
+
+@Repository
+public interface UserService extends JpaRepository<workout,String> {
+    Optional<workout> findById(String id);
+
+
+
+}
